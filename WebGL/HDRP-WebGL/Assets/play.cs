e83ef97191522090f4903075233d4babd8e078d6
wi-photos <wi-photos>
2020-06-14 21:14:17 -0500

    Add WebGL demo

---
+++ WebGL/HDRP-WebGL/Assets/play.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class play : MonoBehaviour {
+    
+    public void TaskOnClick(string level){
+        SceneManager.LoadScene(level);
+
+    }
+}
